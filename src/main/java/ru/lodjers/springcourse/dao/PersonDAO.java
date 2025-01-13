09a9350dc85f93fa636e0aa9827f0d6ed38c2850
lodjers <officialkovtun@gmail.com>
2025-01-13 23:48:29 +0300

    добавили sequence в БД - динамическое инкрементирование id

--- src/main/java/ru/lodjers/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/lodjers/springcourse/dao/PersonDAO.java
@@ -29,7 +29,7 @@ public class PersonDAO {
                 .stream().findAny().orElse(null);
     }
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)",
                 person.getName(), person.getAge(), person.getEmail());
     }
     public void update(int id, Person updatedPerson) throws SQLException {
