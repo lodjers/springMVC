bd40e88fb62d096fbebe3a07ac340cb8c1b8a25c
lodjers <officialkovtun@gmail.com>
2025-02-07 22:41:26 +0300

    сделали CRUD Приложение с Hibernate

--- src/main/java/ru/lodjers/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/lodjers/springcourse/dao/PersonDAO.java
@@ -1,98 +1,54 @@
 package ru.lodjers.springcourse.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ru.lodjers.springcourse.models.Person;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
+
+import java.util.List;
 @Component
 public class PersonDAO {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final SessionFactory sessionFactory;
     @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
-
+    @Transactional
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
-    }
-    public Person show(int id) throws SQLException {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id},
-                        new BeanPropertyRowMapper<>(Person.class))
-                .stream().findAny().orElse(null);
-    }
-    public Optional<Person> show(String email) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[] {email},
-                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
-    }
-    public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES(?, ?, ?, ?)",
-                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
-    }
-    public void update(int id, Person updatedPerson) throws SQLException {
-        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, address=? WHERE id=?",
-                updatedPerson.getName(), updatedPerson.getAge(),
-                updatedPerson.getEmail(), updatedPerson.getAddress(), id);
-    }
-    public void delete(int id) throws SQLException {
-        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
-    }
+        Session session = sessionFactory.getCurrentSession();
 
-    /////////////////// Тест пакетной вставки
+        List<Person> people = session.createQuery("select p from Person p", Person.class).getResultList();
 
-    public void testMultipleUpdate() {
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-
-        for (Person person: people) {
-            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?)", person.getId(), person.getName(),
-                    person.getAge(), person.getEmail());
-        }
-
-        long after = System.currentTimeMillis();
-        System.out.println("Time: " + (after - before));
-    }
-
-    public void testBatchUpdate() {
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-
-        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES(?, ?, ?, ?)",
-                new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
-                        preparedStatement.setInt(1, people.get(i).getId());
-                        preparedStatement.setString(2, people.get(i).getName());
-                        preparedStatement.setInt(3, people.get(i).getAge());
-                        preparedStatement.setString(4, people.get(i).getEmail());
-                    }
-
-                    @Override
-                    public int getBatchSize() {
-                        return people.size();
-                    }
-                });
-
-        long after = System.currentTimeMillis();
-        System.out.println("Time: " + (after - before));
-    }
-    private List<Person> create1000People() {
-        List<Person> people = new ArrayList<>();
-
-        for (int i = 0; i < 1000; i++) {
-            people.add(new Person(i, "Name" + i, "test" + i + "@mail.ru", 30, "some address"));
-        }
         return people;
+    }
+    @Transactional
+    public Person show(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
+    }
+    @Transactional
+    public void save(Person person) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
+    }
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        Session session = sessionFactory.getCurrentSession();
+        Person personToBeUpdated = session.get(Person.class, id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+    }
+    @Transactional
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
+        session.remove(person);
     }
 }
--- src/main/java/ru/lodjers/springcourse/models/Person.java
+++ src/main/java/ru/lodjers/springcourse/models/Person.java
@@ -1,32 +1,28 @@
 package ru.lodjers.springcourse.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
 
-
+@Entity
+@Table(name = "Person")
 public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
-    private String name;
-    @Min(value = 0, message = "Age should be greater than zero")
-    private int age;
-    @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Email should be valid")
-    private String email;
 
-    //Страна, Город, Индекс (6 цифр)
-    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}",
-            message = "Your address should be in this format: Country, City, Postal Code (6 digits)")
-    private String address;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+    
     public Person() {
 
     }
-    public Person(int id, String name, String email, int age, String address) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-        this.email = email;
-        this.address = address;
     }
 
     public int getId() {
@@ -51,18 +47,4 @@ public class Person {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
